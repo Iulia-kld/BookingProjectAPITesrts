bdee79f0af06300620b29e6dd590f52fd0c8ef7c
Julia Bykova <>
2026-04-11 12:58:51 +0200

    Поправила негативные тесты для create booking

--- tests/test_create_booking.py
+++ tests/test_create_booking.py
@@ -13,21 +13,19 @@ def test_create_booking(api_client, generate_random_booking_data):
     assert response["booking"]["lastname"] == generate_random_booking_data["lastname"]
     assert response["booking"]["totalprice"] == generate_random_booking_data["totalprice"]
 
+
 @allure.feature('Test Create Booking')
 @allure.story('Test wrong status code')
-def test_create_booking_wrong_method(api_client, mocker, generate_random_booking_data):
-    mock_response = mocker.Mock()
-    mock_response.status_code = 405
-    mocker.patch.object(api_client.session, 'post', return_value=mock_response)
-    with pytest.raises(AssertionError, match="Expected status code 200 but got 405"):
-        api_client.create_booking(generate_random_booking_data)
+def test_create_booking_wrong_method(api_client, generate_random_booking_data):
+    with pytest.raises(requests.HTTPError):
+        api_client.create_booking({})
 
 
 @allure.feature('Test Create Booking')
-@allure.story('Test wrong URL')
-def test_create_booking_not_found(api_client, mocker, generate_random_booking_data):
-    mock_response = mocker.Mock()
-    mock_response.status_code = 404
-    mocker.patch.object(api_client.session, 'post', return_value=mock_response)
-    with pytest.raises(AssertionError, match="Expected status code 200 but got 404"):
-        api_client.create_booking(generate_random_booking_data)
+@allure.story('Test wrong body')
+def test_create_booking_missing_field(api_client, generate_random_booking_data):
+    bad_date = generate_random_booking_data.copy()
+    bad_date.pop("firstname")
+    # response.status_code = 500
+    with pytest.raises(requests.HTTPError):
+        api_client.create_booking(bad_date)
